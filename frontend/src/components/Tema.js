3c9243e8c63bfab1129b73f350a374886901f44f
Valentina Salazar Arango <66386749+valensalazar03@users.noreply.github.com>
2020-09-29 20:09:09 -0500

    Adición de tema 1

---
+++ frontend/src/components/Tema.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Tema = (props) => {
+  const { history } = props;
+  return (
+    <div>
+      <p className="text-break">
+        <h4 align="left">Envases de plástico</h4>
+        <img className="Primera" src="./img/plastico.jpg" align="left" />
+        Reducir suconsumo, reutilizar y reciclar son las tres acciones básicas
+        para reducir su impacto, ya que el plástico tarda alrededor de 700 años
+        en degradarse. Por eso, es importante depositar en el contenedor
+        amarillo los envases reciclables como:
+      </p>
+      <ul>
+        <li>Botellas de plástico</li>
+        <li>Bote de detergente, champú, gel</li>
+        <li>Las bolsas de patatas</li>
+        <li>Tapas y tapones de plástico</li>
+        <li>Bolsas y plástico (excepto las bolsas de basura)</li>
+        <li>Tarrinas y tapas de yogurt</li>
+      </ul>
+      <div>
+        <p className="text-break">
+          <br></br>
+          <h4 align="left">Envases de papel y cartón</h4>
+          <img className="Primera" src="./img/plasticodos.jpg" align="left" />
+          Todos los materiales de pael y cartón deben ir al contenedor azul para
+          ser reciclados y ayudar a reducir la sobreexplotación de los recursos
+          naturales, algunos de estos son:
+        </p>
+        <ul>
+          <li>Revistas</li>
+          <li>Periodicos viejos</li>
+          <li>Cajas de cereales</li>
+          <li>Cajas de zapatos</li>
+          <li>Papel de envolver</li>
+          <li>Cuadernos</li>
+        </ul>
+      </div>
+
+      <div className="Centrado">
+        <button
+          type="submit"
+          className="btn btn-danger"
+          onClick={() => history.goBack("/")}
+        >
+          Volver
+        </button>
+
+        <button
+          type="submit"
+          className="btn btn-info"
+          onClick={() => history.push("/Temados")}
+        >
+          Siguiente
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Tema;
---
+++ frontend/src/components/Tematres.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+
+const Tematres = (props) => {
+  const {history} = props;
+  return(
+    <div>
+      <p className="text-break">
+      <h4 align="left">Vidrio</h4>
+      <img className="Primera" src="./img/plasticocinco.jpg" align="left"/>
+        Este material se puede volver a reutilizar mediante un proceso de lavado de desechos a través de su fundición y puede ser reciclado al 100% una cantidad indefinida de veces. Para ello debemos separarlos del resto de materiales. ¡Al contenedor verde! Aquí debemos depositar: 
+
+        </p> 
+        <ul>
+    <li>Botellas de vidrio (vinos, cava...)</li>
+    <li>Frascos de vidrio (como perfumes y colonias)</li>
+    <li>Tarros de alimentos (mermeladas, conservas, etc...)</li>
+    
+</ul>
+<div>
+
+<p className="text-break">
+      <h4 align="left">Pilas y baterías</h4>
+      <img className="Primera" src="./img/plasticoseis.jpg" align="left"/>
+        Podemos encontrarlas a diario en el control de la tele, el ratón del ordenador o en la maquinilla de afeitar y nunca debemos tirarlas a la basura con el restro de residuos, Tienen componentes altamente contamiantes, por lo que es muy importante llevarlas a los contenedores de residuos peligrosos que tengamos más cerca de casa o al punto limpio.
+
+        </p> 
+
+</div>
+
+<div>
+      <p className="text-break">
+      <h4 align="left">Otros</h4>
+      <img className="Primera" src="./img/plasticocinco.jpg" align="left"/>
+        Los siguientes productos y muchos otros también se pueden reciclar gracias a los puntos limpios. La ropa tambíen es reciclable, simplemente habrá que llevarla a los puntos habilitados para ello.
+
+        </p> 
+        <ul>
+    <li>Electrodomésticos</li>
+    <li>Aparatos electrónicos</li>
+    <li>Bonbillas</li>
+    <li>Aceites usados</li>
+    <li>Muebles</li>
+</ul>
+</div>
+
+<div>
+          <button type="button" className="btn btn-danger"
+          onClick={() => history.goBack("/")}
+          >
+          Volver
+          </button>
+
+          </div>
+ 
+ 
+
+
+      </div>
+    
+  )
+}
+export default Tematres;
