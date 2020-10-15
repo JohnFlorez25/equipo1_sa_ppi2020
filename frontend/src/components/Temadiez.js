8ed7a1b617db36c274477cee6d02a734801553d5
Valentina Salazar Arango <66386749+valensalazar03@users.noreply.github.com>
2020-10-14 19:45:00 -0500

    Adición tema 5

---
+++ frontend/src/components/Temadiez.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../styles/Temadiez.css";
+
+const Temadiez = (props) => {
+  const { history } = props;
+  return(
+    <div className="orga">
+      <p className="text-center">
+        
+        <br></br>
+        
+
+        <img className="perso" src="./img/personas.jpg"/><br></br><br></br>
+        El destino que le damos a los materiales que pueden ser reciclados o reutilizados es otra buena forma de contribuir a la sustentabilidad significativamente, por ello la importancia de hacer una buena separación y recolección de nuestros desechos es vital.       
+        <br></br><br></br>
+        </p>
+        <h4 align="left">Orgánica</h4>
+        <img className="dese" src="./img/organica.png" align="left" />
+        Son desperdicios de alimentos como cascaras de fruta y verdura o cualquier otro alimento (no carnes), cascaron de huevo y desechos de jardin. La finalidad de esta basura, es la elaboración de compotas, abonos y gas natural a través de la biotenología. 
+      
+        
+
+        <div><br></br><br></br><br></br>
+          <button type="button" className="btn btn-danger"
+          onClick={() => history.goBack("/")}
+          >
+          Volver
+          </button>
+          <button type="button" className="btn btn-info"
+          onClick={() => history.push("/Temaonce")}
+          >
+          Siguiente
+          </button>
+
+          </div>
+          
+    </div>
+  );
+};
+
+export default Temadiez;
---
+++ frontend/src/components/Temaonce.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../styles/Temaonce.css";
+
+const Temaonce = (props) => {
+  const { history } = props;
+  return (
+    <div className="ino">
+      <p className="text"><br></br>
+        <h4 align="left">Inorgánica</h4>
+        <img className="ano" src="./img/inorganica.png" align="left" />
+       Los residuos inorgánicos son aquellos de orígen no biológico, de orígen industrial o de algún proceso no natural, que expuestos a las condiciones ambientales naturales, tarda mucho tiempo en degradarse, es decir, no vuelven a integrarse a la tierra, sino tras un largo periodo de tiempo. La finalidad de dichos desperdicios es la elaboración de productos como envases, piezas automotrices, productos decorativos, etc...  
+      </p>
+      
+      
+        
+          <h4 align="left">PET</h4>
+          <img className="ano" src="./img/pet.gif" align="left" />
+          Todos los materiales de pael y cartón deben ir al contenedor azul para
+          ser reciclados y ayudar a reducir la sobreexplotación de los recursos
+          naturales, algunos de estos son:
+        
+        
+      <br></br><br></br><br></br><br></br>
+
+      <div className="Centrado">
+        <button
+          type="submit"
+          className="btn btn-danger"
+          onClick={() => history.goBack("/")}
+        >
+          Volver
+        </button>
+
+        
+      </div>
+    </div>
+  );
+};
+export default Temaonce;
